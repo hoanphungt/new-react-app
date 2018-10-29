347ad12ca59098bc49755f257a9f8cbd04179099
Hoan Phung <hoanphung.ptso1@gmail.com>
2018-10-29 16:18:01 +0100

    new welcome

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to React! My name is Hoan
+            Welcome to Codaisseur!
           </p>
           
         </header>
